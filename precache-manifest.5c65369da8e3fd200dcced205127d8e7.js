7d29d63b3678f2af54792f4a9e98797b296ea174
Ashish <ashishmp0471@gmail.com>
2020-07-05 10:21:40 +0530

    Updates

---
+++ precache-manifest.5c65369da8e3fd200dcced205127d8e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a13a0e543e9508cbc40c407536d2ee8",
+    "url": "/avatar/index.html"
+  },
+  {
+    "revision": "188c0dbb3a015859d817",
+    "url": "/avatar/static/css/main.eca1484d.chunk.css"
+  },
+  {
+    "revision": "5ba99fb6fb5da084b675",
+    "url": "/avatar/static/js/2.def314a4.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/avatar/static/js/2.def314a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "188c0dbb3a015859d817",
+    "url": "/avatar/static/js/main.e42ce218.chunk.js"
+  },
+  {
+    "revision": "909dc484e9e56f346ebe",
+    "url": "/avatar/static/js/runtime-main.0f73d954.js"
+  }
+]);
